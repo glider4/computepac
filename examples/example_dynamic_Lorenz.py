c31034299fd333f7dbb3bfdafc69128c91965009
mathemacode <43391344+mathemacode@users.noreply.github.com>
2019-12-13 16:29:53 -0500

    Build lorenz example, upload resulting plot

--- examples/example_dynamic_Lorenz.py
+++ examples/example_dynamic_Lorenz.py
@@ -1,24 +1,18 @@
 # Example of the Lorenz Attractor solved by Adams-Bashforth-Moulton
 # in the dynamic_triple module
+#
+# Should be run in an IDE like Anaconda.  May not work with PyCharm.
 
 from computepac import *
 
+# Lorenz Attractor equations
+def f1(x, y, z):
+    return 10 * (y - x)
 
-def lorenz(a, b, n):
-    def f1(x, y, z):
-        return 10 * (y - x)
+def f2(x, y, z):
+    return x * (28 - z) - y
 
+def f3(x, y, z):
+    return (x * y) - (8/3)*z
 
-    def f2(x, y, z):
-        return x * (28 - z) - y
-
-
-    def f3(x, y, z):
-        return (x * y) - (8/3)*z
-
-
-    abm_comp(f1, f2, f3, 0, 15, 15, 36, a, b, n)
-
-
-if __name__ == '__main__':
-    lorenz(0, 20, 1000)
+abm_comp(f1, f2, f3, 0, 15, 15, 36, 0, 20, 1000)
