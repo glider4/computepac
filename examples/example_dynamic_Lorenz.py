2312a16a95a3fcea320102a10667e264a4cd7e14
mathemacode <43391344+mathemacode@users.noreply.github.com>
2021-04-16 21:07:39 -0400

    Make dynamic_triple a Class; refactor entire ABM code

--- examples/example_dynamic_Lorenz.py
+++ examples/example_dynamic_Lorenz.py
@@ -1,9 +1,8 @@
 # Example of the Lorenz Attractor solved by Adams-Bashforth-Moulton
 # in the dynamic_triple module
 #
-# Should be run in an IDE like Anaconda.  May not work with PyCharm.
 
-from computepac import *
+from computepac.dynamic_triple import ABM
 
 
 # Lorenz Attractor equations
@@ -19,4 +18,5 @@ def f3(x, y, z):
     return (x * y) - (8 / 3) * z
 
 
-abm_comp(f1, f2, f3, 0, 15, 15, 36, 0, 20, 1000)
+res = ABM(f1, f2, f3, 0, 15, 15, 36, 0, 20, 1000)
+print(res.adams()[2][100])
