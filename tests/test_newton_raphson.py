fd1b30ab1b46fa3932794d8e9cb8e0c4add19cef
mathemacode <43391344+mathemacode@users.noreply.github.com>
2021-04-16 20:12:55 -0400

    Update tests with Utilities function

--- tests/test_newton_raphson.py
+++ tests/test_newton_raphson.py
@@ -1,5 +1,6 @@
 import sympy as sym
 from computepac import newton_raphson
+from computepac.utils.utilities import Utilities
 
 
 class Test:
@@ -7,4 +8,5 @@ class Test:
         f = sym.Function('f')
         x = sym.Symbol('x')
         f = (x ** 3) + 2 * (x ** 2) + 10 * x - 20
-        assert newton_raphson(f, 2, 0.5e-12)[0] == 6
+        ans = newton_raphson(f, 2, 0.5e-12)[0]
+        assert Utilities.check_same_float(ans, 6.0, 1) is True
