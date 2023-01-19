64219769342355c718c3b3faa32d5562667d52dc
mathemacode <43391344+glider4@users.noreply.github.com>
2023-01-19 22:28:33 +0100

    fix: link

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="computepac",
     version="1.5",
     description="A Python package for numerical analysis: root finding, iterative solvers, other algorithms.",
-    url="https://github.com/mathemacode/computepac",
+    url="https://github.com/glider4/computepac",
     author="glider4",
     classifiers=["License :: GNU GPLv3" "Programming Language :: Python 3.7"],
     keywords="computational mathematics engineering numerical analysis finite difference iterative schemes root finding algorithms",
